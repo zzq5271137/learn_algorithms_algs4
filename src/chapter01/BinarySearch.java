ca63428d542ea8d14b9b881339b0ff5d4cba6dde
ZHENGQING ZHU <zhuzhengqing321@qq.com>
2020-03-16 00:52:38 +0800

    no message

---
+++ src/chapter01/BinarySearch.java
@@ -0,0 +1,36 @@
+package chapter01;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] whitelist = new In(args[0]).readAllInts();
+        Arrays.sort(whitelist);
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (rank(key, whitelist) == -1) {
+                StdOut.println(key);
+            }
+        }
+    }
+
+    public static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) {
+                hi = mid - 1;
+            } else if (key > a[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
